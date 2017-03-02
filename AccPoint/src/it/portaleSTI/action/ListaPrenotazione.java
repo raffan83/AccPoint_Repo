02b79e097f35fe94bc4dde00fd74686af53a7095
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-03-02 10:08:29 +0100

    comm

---
+++ AccPoint/src/it/portaleSTI/action/ListaPrenotazione.java
@@ -0,0 +1,40 @@
+package it.portaleSTI.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListaPrenotazione
+ */
+@WebServlet(name = "listaPrenotazioni", urlPatterns = { "/listaPrenotazioni.do" })
+
+public class ListaPrenotazione extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListaPrenotazione() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}

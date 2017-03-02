d972beb2913b529ae0e228e6034f1c39a6c59ecb
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-03-02 10:33:29 +0100

    ok

--- AccPoint/src/it/portaleSTI/DTO/PrenotazioneDTO.java
+++ AccPoint/src/it/portaleSTI/DTO/PrenotazioneDTO.java
@@ -5,13 +5,18 @@ import java.sql.Date;
 public class PrenotazioneDTO {
 	private int id;
 	private int id_campione;
-	private int id_company;
-	private int id_user;
+	private int id_companyRichiedente;
+	private int id_userRichiedente;
+	private Date dataRichiesta;
+	private Date dataApprovazione;
 	private int stato;
 	private Date prenotatoDal;
 	private Date prenotatoAl;
 	private String note;
-	
+	private String nomeCampione;
+	private String matricolaCampione;
+	private int id_company;
+	private int id_company_utilizzatrice;
 	
 	public PrenotazioneDTO() {
 	}
@@ -22,8 +27,8 @@ public class PrenotazioneDTO {
 	}
 
 
-	public void setId_prenotazione(int id_) {
-		this.id = id_;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 
@@ -37,23 +42,43 @@ public class PrenotazioneDTO {
 	}
 
 
-	public int getId_company() {
-		return id_company;
+	public int getId_companyRichiedente() {
+		return id_companyRichiedente;
 	}
 
 
-	public void setId_company(int id_company) {
-		this.id_company = id_company;
+	public void setId_companyRichiedente(int id_companyRichiedente) {
+		this.id_companyRichiedente = id_companyRichiedente;
 	}
 
 
-	public int getId_user() {
-		return id_user;
+	public int getId_userRichiedente() {
+		return id_userRichiedente;
 	}
 
 
-	public void setId_user(int id_user) {
-		this.id_user = id_user;
+	public void setId_userRichiedente(int id_userRichiedente) {
+		this.id_userRichiedente = id_userRichiedente;
+	}
+
+
+	public Date getDataRichiesta() {
+		return dataRichiesta;
+	}
+
+
+	public void setDataRichiesta(Date dataRichiesta) {
+		this.dataRichiesta = dataRichiesta;
+	}
+
+
+	public Date getDataApprovazione() {
+		return dataApprovazione;
+	}
+
+
+	public void setDataApprovazione(Date dataApprovazione) {
+		this.dataApprovazione = dataApprovazione;
 	}
 
 
@@ -94,6 +119,46 @@ public class PrenotazioneDTO {
 
 	public void setNote(String note) {
 		this.note = note;
+	}
+
+
+	public String getNomeCampione() {
+		return nomeCampione;
+	}
+
+
+	public void setNomeCampione(String nomeCampione) {
+		this.nomeCampione = nomeCampione;
+	}
+
+
+	public String getMatricolaCampione() {
+		return matricolaCampione;
+	}
+
+
+	public void setMatricolaCampione(String matricolaCampione) {
+		this.matricolaCampione = matricolaCampione;
+	}
+
+
+	public int getId_company() {
+		return id_company;
+	}
+
+
+	public void setId_company(int id_company) {
+		this.id_company = id_company;
+	}
+
+
+	public int getId_company_utilizzatrice() {
+		return id_company_utilizzatrice;
+	}
+
+
+	public void setId_company_utilizzatrice(int id_company_utilizzatrice) {
+		this.id_company_utilizzatrice = id_company_utilizzatrice;
 	}
 
 	

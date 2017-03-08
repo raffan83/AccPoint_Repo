231432da6aaa9a4d9aee54f351cb6edbcea2473c
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-03-08 09:11:03 +0100

    com

---
+++ AccPoint/src/it/portaleSTI/DTO/StatoPrenotazioneDTO.java
@@ -0,0 +1,29 @@
+package it.portaleSTI.DTO;
+
+public class StatoPrenotazioneDTO {
+
+	private int id;
+	private String descrizione;
+	
+	public StatoPrenotazioneDTO() {
+	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	
+}

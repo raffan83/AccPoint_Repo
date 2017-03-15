fe691b63067ac601cdce1988f23b94710daefb5a
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-03-15 08:45:28 +0100

    com const

--- AccPoint/src/it/portaleSTI/Util/Costanti.java
+++ AccPoint/src/it/portaleSTI/Util/Costanti.java
@@ -1,5 +1,7 @@
 package it.portaleSTI.Util;
 
+import java.util.Properties;
+
 public class Costanti {
 
 	public static final String PATH_FOLDER="C:\\Users\\raffaele.fantini\\temp\\";
@@ -11,5 +13,8 @@ public class Costanti {
 	public static final String FILE_NAME_LS_SCH_STR = "CORE\\scheda_strumento.txt";
 	public static final String STORAGE_ZIP_PATH="\\\\192.168.3.102\\Backup\\Raffaele Fantini\\CALVER";
 	public static final String CON_STR_MYSQL = "jdbc:mysql://192.168.12.4:3306/gtv_db?user=root&password=root";
+	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://158.58.172.111:1433;databaseName=BTOMEN_CRESCO_DATI";
+	public static final String USR_SQL_SVR = "fantini";
+	public static final String USR_PASS_SVR = "fantini";
 			
 }

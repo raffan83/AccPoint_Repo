be8fc400c1e5159e779a918bd748590fae9a00cf
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-03-15 16:21:57 +0100

    commit session factory

--- AccPoint/src/it/portaleSTI/DAO/SessionFacotryDAO.java
+++ AccPoint/src/it/portaleSTI/DAO/SessionFacotryDAO.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class SessionFacotryDAO {
-	private static SessionFactory sessionFactory;
+	/*private static SessionFactory sessionFactory;
 	
 	public static SessionFactory get() throws Exception {
 //		Otteniamo una SessionFactory per la nostra applicazione
@@ -16,6 +16,30 @@ public class SessionFacotryDAO {
 		{
 			sf.close();
 		}
-	}
+	}*/
 
+	 private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	    private static SessionFactory buildSessionFactory() {
+	        try {
+	            // Create the SessionFactory from hibernate.cfg.xml
+	            return new Configuration().configure().buildSessionFactory();
+	        }
+	        catch (Throwable ex) {
+	            // Make sure you log the exception, as it might be swallowed
+	            System.err.println("Initial SessionFactory creation failed." + ex);
+	            throw new ExceptionInInitializerError(ex);
+	        }
+	    }
+
+	    public static SessionFactory get() {
+	        return sessionFactory;
+	    }
+	    
+	    public static void shutDown(SessionFactory sf) throws Exception {
+			if ( sf != null ) 
+			{
+				sf.close();
+			}
+	    }
 }

e5c901e898907d62f9a78cf6eccc838012b7cdfe
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-03-24 09:25:37 +0100

    a

---
+++ AccPoint/src/it/portaleSTI/DTO/MisuraDTO.java
@@ -0,0 +1,85 @@
+package it.portaleSTI.DTO;
+
+public class MisuraDTO {
+	
+	int tipoProva;
+	String tipoVerifica;
+	String um;
+	String valoreCampione;
+	String valoreMedioCampione;
+	String valoreStrumento;
+	String valoreMedioStrumento;
+	String scostamento;
+	String accettabilità;
+	String incertezza;
+	String esito;
+	
+	public int getTipoProva() {
+		return tipoProva;
+	}
+	public void setTipoProva(int tipoProva) {
+		this.tipoProva = tipoProva;
+	}
+	public String getTipoVerifica() {
+		return tipoVerifica;
+	}
+	public void setTipoVerifica(String tipoVerifica) {
+		this.tipoVerifica = tipoVerifica;
+	}
+	public String getUm() {
+		return um;
+	}
+	public void setUm(String um) {
+		this.um = um;
+	}
+	public String getValoreCampione() {
+		return valoreCampione;
+	}
+	public void setValoreCampione(String valoreCampione) {
+		this.valoreCampione = valoreCampione;
+	}
+	public String getValoreMedioCampione() {
+		return valoreMedioCampione;
+	}
+	public void setValoreMedioCampione(String valoreMedioCampione) {
+		this.valoreMedioCampione = valoreMedioCampione;
+	}
+	public String getValoreStrumento() {
+		return valoreStrumento;
+	}
+	public void setValoreStrumento(String valoreStrumento) {
+		this.valoreStrumento = valoreStrumento;
+	}
+	public String getValoreMedioStrumento() {
+		return valoreMedioStrumento;
+	}
+	public void setValoreMedioStrumento(String valoreMedioStrumento) {
+		this.valoreMedioStrumento = valoreMedioStrumento;
+	}
+	public String getScostamento() {
+		return scostamento;
+	}
+	public void setScostamento(String scostamento) {
+		this.scostamento = scostamento;
+	}
+	public String getAccettabilità() {
+		return accettabilità;
+	}
+	public void setAccettabilità(String accettabilità) {
+		this.accettabilità = accettabilità;
+	}
+	public String getIncertezza() {
+		return incertezza;
+	}
+	public void setIncertezza(String incertezza) {
+		this.incertezza = incertezza;
+	}
+	public String getEsito() {
+		return esito;
+	}
+	public void setEsito(String esito) {
+		this.esito = esito;
+	}
+	
+
+}

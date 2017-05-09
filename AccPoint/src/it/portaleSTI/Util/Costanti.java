7c7c77ec456cb393706066fc299637a8c852973f
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-05-09 08:58:36 +0200

    Cost

--- AccPoint/src/it/portaleSTI/Util/Costanti.java
+++ AccPoint/src/it/portaleSTI/Util/Costanti.java
@@ -3,14 +3,7 @@ package it.portaleSTI.Util;
 
 public class Costanti {
 
-	public static final String PATH_FOLDER="C:\\Users\\raffaele.fantini\\temp\\";
-	//public static final String PATH_FOLDER="/Users/marcopagnanelli/gitSite/AccPoint/AccPoint/WebContent/upload/";
-	public static final String PATH_SOURCE_FORM = "C:\\Users\\raffaele.fantini\\temp\\source_template";
-	public static final String FILE_NAME_LS_STR = "CORE\\listaStrumenti.txt";
-	public static final String FILE_NAME_LS_CMP = "CORE\\ListaCampioni.txt";
-	public static final String FILE_NAME_LS_CMP_STR = "CORE\\listaCPS.txt";
-	public static final String FILE_NAME_LS_SCH = "CORE\\listaScheda.txt";
-	public static final String FILE_NAME_LS_SCH_STR = "CORE\\scheda_strumento.txt";
+	public static final String PATH_FOLDER="\\\\192.168.3.102\\Backup\\Raffaele Fantini\\Repository\\";
 	public static final String STORAGE_ZIP_PATH="\\\\192.168.3.102\\Backup\\Raffaele Fantini\\CALVER";
 	public static final String CON_STR_MYSQL = "jdbc:mysql://192.168.12.4:3306/gtv_db?user=root&password=root";
 	public static final String CON_STR_SQLSRV = "jdbc:sqlserver://158.58.172.111:1433;databaseName=BTOMEN_CRESCO_DATI";

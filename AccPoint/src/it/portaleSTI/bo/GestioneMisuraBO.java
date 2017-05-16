7a04647820a3fa33aff942dd6ab8144a7205c4bd
Marco Pagnanelli <marcopagnanelli@markpagna-MacBook-Pro.local>
2017-05-16 11:53:26 +0200

    no message

--- AccPoint/src/it/portaleSTI/bo/GestioneMisuraBO.java
+++ AccPoint/src/it/portaleSTI/bo/GestioneMisuraBO.java
@@ -1,6 +1,8 @@
 package it.portaleSTI.bo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -9,6 +11,7 @@ import it.portaleSTI.DAO.GestioneInterventoDAO;
 import it.portaleSTI.DAO.GestioneMisuraDAO;
 import it.portaleSTI.DAO.SessionFacotryDAO;
 import it.portaleSTI.DTO.MisuraDTO;
+import it.portaleSTI.DTO.PuntoMisuraDTO;
 
 public class GestioneMisuraBO {
 
@@ -18,4 +21,46 @@ public class GestioneMisuraBO {
 			return GestioneMisuraDAO.getMiruraByID(idMisura);
 		
 	}
+	
+	
+	public static int getTabellePerMisura(Set<PuntoMisuraDTO> listaPunti) {
+		
+		  int index=0;
+		  Iterator<PuntoMisuraDTO> iterator = listaPunti.iterator(); 
+	      
+		   // check values
+		   while (iterator.hasNext())
+		   
+		   {
+		    PuntoMisuraDTO punto = (PuntoMisuraDTO) iterator.next();	   
+		    
+		    if(punto.getId_tabella()!=index)
+		    {
+		    	index++;
+		    }
+		   }
+		return index;
+	}
+	
+	public static ArrayList<PuntoMisuraDTO> getListaPuntiByIdTabella(Set<PuntoMisuraDTO> listaPunti, int idTabella) {
+		  ArrayList<PuntoMisuraDTO> listaPuntiMisura = new ArrayList<>();
+		  
+		  
+		  Iterator<PuntoMisuraDTO> iterator = listaPunti.iterator(); 
+	      
+		   // check values
+		   while (iterator.hasNext())
+		   
+		   {
+		    PuntoMisuraDTO punto = (PuntoMisuraDTO) iterator.next();	   
+		    
+		    if(punto.getId_tabella()==idTabella)
+		    {
+		    	listaPuntiMisura.add(punto);
+		    }
+		   }
+		return listaPuntiMisura;
+	}
+
+
 }

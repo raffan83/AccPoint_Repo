851a46170e8f162acb4d77df872280c79f68a092
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-05-16 14:36:07 +0200

    committ

--- AccPoint/src/it/portaleSTI/bo/GestioneMisuraBO.java
+++ AccPoint/src/it/portaleSTI/bo/GestioneMisuraBO.java
@@ -1,9 +1,14 @@
 package it.portaleSTI.bo;
 
+import it.portaleSTI.DAO.GestioneCampioneDAO;
+import it.portaleSTI.DTO.CampioneDTO;
 import it.portaleSTI.DTO.PuntoMisuraDTO;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class GestioneMisuraBO {
@@ -46,6 +51,44 @@ public class GestioneMisuraBO {
 		    }
 		   }
 		return listaPuntiMisura;
+	}
+
+	public static List<CampioneDTO> getListaCampioni(Set<PuntoMisuraDTO> listaPunti) {
+	
+		List<CampioneDTO> listToReturn = new ArrayList();
+		
+		HashMap<String,String> listaCampioni = new HashMap<>();
+		
+		Iterator<PuntoMisuraDTO> iterator = listaPunti.iterator(); 
+	      
+		   // check values
+		   while (iterator.hasNext())
+		   
+		   {
+		    PuntoMisuraDTO punto = (PuntoMisuraDTO) iterator.next();	   
+		    
+		    	if(!listaCampioni.containsKey(punto.getDesc_Campione()))
+		    	{
+		    		listaCampioni.put(punto.getDesc_Campione(), punto.getDesc_Campione());
+		    	}
+		    	
+		    
+		    
+		   }
+		   
+		   Iterator itCampioni=listaCampioni.entrySet().iterator();
+		   
+		   while (itCampioni.hasNext()) {
+		        
+			   Map.Entry pair = (Map.Entry)itCampioni.next();
+		        
+		        CampioneDTO campione =GestioneCampioneDAO.getCampioneFromCodice(pair.getKey().toString());
+		       
+		        listToReturn.add(campione);
+		        itCampioni.remove(); // avoids a ConcurrentModificationException
+		    }
+		   
+		return listToReturn;
 	}
 
 }

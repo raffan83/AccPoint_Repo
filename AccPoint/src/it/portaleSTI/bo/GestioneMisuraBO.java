5a05ef06eeb2de948490f91dd46bf84839ad99f7
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-05-16 11:48:35 +0200

    commit

--- AccPoint/src/it/portaleSTI/bo/GestioneMisuraBO.java
+++ AccPoint/src/it/portaleSTI/bo/GestioneMisuraBO.java
@@ -1,5 +1,51 @@
 package it.portaleSTI.bo;
 
+import it.portaleSTI.DTO.PuntoMisuraDTO;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
 public class GestioneMisuraBO {
+
+	
+	public static int getTabellePerMisura(Set<PuntoMisuraDTO> listaPunti) {
+		
+		  int index=0;
+		  Iterator<PuntoMisuraDTO> iterator = listaPunti.iterator(); 
+	      
+		   // check values
+		   while (iterator.hasNext())
+		   
+		   {
+		    PuntoMisuraDTO punto = (PuntoMisuraDTO) iterator.next();	   
+		    
+		    if(punto.getId_tabella()!=index)
+		    {
+		    	index++;
+		    }
+		   }
+		return index;
+	}
+	
+	public static ArrayList<PuntoMisuraDTO> getListaPuntiByIdTabella(Set<PuntoMisuraDTO> listaPunti, int idTabella) {
+		  ArrayList<PuntoMisuraDTO> listaPuntiMisura = new ArrayList<>();
+		  
+		  
+		  Iterator<PuntoMisuraDTO> iterator = listaPunti.iterator(); 
+	      
+		   // check values
+		   while (iterator.hasNext())
+		   
+		   {
+		    PuntoMisuraDTO punto = (PuntoMisuraDTO) iterator.next();	   
+		    
+		    if(punto.getId_tabella()==idTabella)
+		    {
+		    	listaPuntiMisura.add(punto);
+		    }
+		   }
+		return listaPuntiMisura;
+	}
 
 }

7751a73d6b598712685fafb799858aec458f61c4
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2017-05-30 14:03:46 +0200

    commit

---
+++ AccPoint/src/it/portaleSTI/DTO/AttivitaMilestoneDTO.java
@@ -0,0 +1,35 @@
+package it.portaleSTI.DTO;
+
+public class AttivitaMilestoneDTO {
+
+	private String descrizioneAttivita;
+	private String noteAttivita;
+	private String descrizioneArticolo;
+	private String quantita;
+	public String getDescrizioneAttivita() {
+		return descrizioneAttivita;
+	}
+	public void setDescrizioneAttivita(String descrizioneAttivita) {
+		this.descrizioneAttivita = descrizioneAttivita;
+	}
+	public String getNoteAttivita() {
+		return noteAttivita;
+	}
+	public void setNoteAttivita(String noteAttivita) {
+		this.noteAttivita = noteAttivita;
+	}
+	public String getDescrizioneArticolo() {
+		return descrizioneArticolo;
+	}
+	public void setDescrizioneArticolo(String descrizioneArticolo) {
+		this.descrizioneArticolo = descrizioneArticolo;
+	}
+	public String getQuantita() {
+		return quantita;
+	}
+	public void setQuantita(String quantita) {
+		this.quantita = quantita;
+	}
+	
+	
+}
---
+++ AccPoint/src/it/portaleSTI/action/CaricaPacchettoDuplicati.java
@@ -0,0 +1,102 @@
+package it.portaleSTI.action;
+
+import it.portaleSTI.DAO.GestioneInterventoDAO;
+import it.portaleSTI.DAO.SessionFacotryDAO;
+import it.portaleSTI.DTO.InterventoDTO;
+import it.portaleSTI.DTO.ObjSavePackDTO;
+import it.portaleSTI.DTO.UtenteDTO;
+import it.portaleSTI.Util.Utility;
+import it.portaleSTI.bo.GestioneInterventoBO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+
+import com.google.gson.JsonObject;
+
+@WebServlet(name= "/caricaPacchettoDuplicati", urlPatterns = { "/caricaPacchettoDuplicati.do" })
+
+public class CaricaPacchettoDuplicati extends HttpServlet {
+
+	public CaricaPacchettoDuplicati() {
+		super();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		if(Utility.validateSession(request,response,getServletContext()))return;
+
+		ObjSavePackDTO esito=null;
+		JsonObject jsono = new JsonObject();
+		PrintWriter writer = response.getWriter();
+
+
+		InterventoDTO intervento= (InterventoDTO)request.getSession().getAttribute("intervento");
+		UtenteDTO utente =(UtenteDTO)request.getSession().getAttribute("userObj");
+
+		Session session=SessionFacotryDAO.get().openSession();
+		session.beginTransaction();
+
+
+		try
+		{
+			String obj =request.getParameter("ids");
+
+			esito =(ObjSavePackDTO)request.getSession().getAttribute("esito");	
+
+			if(obj!=null && obj.length()>0)
+			{
+				String[] lista =obj.split(",");
+
+				for (int i = 0; i < lista.length; i++) 
+				{
+					GestioneInterventoBO.updateMisura(lista[i],esito,intervento,utente,session);
+
+					intervento.setnStrumentiMisurati(intervento.getnStrumentiMisurati()+1);
+					esito.getInterventoDati().setNumStrMis(esito.getInterventoDati().getNumStrMis()+1);
+
+					GestioneInterventoBO.updateInterventoDati(esito.getInterventoDati(),session);
+					GestioneInterventoBO.update(intervento, session);
+
+				}
+
+				jsono.addProperty("success", true);
+				jsono.addProperty("messaggio", "Sono stati salvati "+esito.getInterventoDati().getNumStrMis()+" \n"+"Nuovi Strumenti: "+esito.getInterventoDati().getNumStrNuovi());
+
+			}
+			else
+			{
+
+				if(esito.getInterventoDati().getNumStrMis()==0)
+				{
+					jsono.addProperty("messaggio","");
+					GestioneInterventoBO.removeInterventoDati(esito.getInterventoDati(),session);
+					jsono.addProperty("success", true);
+				}
+			}
+			session.getTransaction().commit();
+			session.close();
+
+
+
+		}catch (Exception e) {
+
+
+			session.getTransaction().rollback();
+			jsono.addProperty("success", false);
+			jsono.addProperty("messaggio", "Errore importazione pacchetto [Duplicato]"+e.getMessage());
+
+		}
+		writer.write(jsono.toString());
+		writer.close();
+	} 
+}
+

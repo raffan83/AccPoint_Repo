cd73d88121be7573ab9bd3d020b22933696dcb89
Marco Pagnanelli <marcopagnanelli@markpagna-MacBook-Pro.local>
2017-10-27 16:23:55 +0200

    no message

--- AccPoint/src/it/portaleSTI/Util/CostantiCertificato.java
+++ AccPoint/src/it/portaleSTI/Util/CostantiCertificato.java
@@ -5,7 +5,7 @@ public class CostantiCertificato {
 	public static final String FOOTER_RIGHT="Rev. A del 01/06/2011";
 	public static final String TITOLO_LISTA_CAMPIONI="Riferimenti Utilizzati e Metodo di Taratura";
 	public static final String TITOLO_LISTA_MISURE="RISULTATI DELLA VERIFICA DI TARATURA";
-	public static final String DESCRIZIONE_INCERTEZZA = "L'incertezze di misura dichiarate in questo documento sono espresse come due volte lo scarto tipo (corrispondente, nel caso di distribuzione normale, ad un livellodi confidenza di circa 95%)";
+	public static final String DESCRIZIONE_INCERTEZZA = "L'incertezze di misura dichiarate in questo documento sono espresse come due volte lo scarto tipo (corrispondente, nel caso di distribuzione normale, ad un livello di confidenza di circa 95%)";
 	public static final String ESITO_TITLE = "Esito della verifica:";
 	public static final String ACCETTABILITA_DESC = "(U <= Accettabilità )";
 	public static final String OPERATORE_LABEL = "Operatore (OT)";

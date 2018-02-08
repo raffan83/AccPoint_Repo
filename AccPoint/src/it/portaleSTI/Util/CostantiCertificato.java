bcfbc076f134cf56777d20ec478c70db13e0a8b8
Marco Pagnanelli <marcopagnanelli@markpagna-MacBook-Pro.local>
2018-02-08 12:35:07 +0100

    Fix schedaConsegna Metrologia

--- AccPoint/src/it/portaleSTI/Util/CostantiCertificato.java
+++ AccPoint/src/it/portaleSTI/Util/CostantiCertificato.java
@@ -22,5 +22,6 @@ public class CostantiCertificato {
 	public static final String FOOTER_LEFT_07="MOD-PGI016-07";
 	public static final String FOOTER_LEFT_08="MOD-PGI016-08";
 	public static final String FOOTER_RIGHT="Rev. 0 del 08/01/2018";
+	public static final String NOTA_CONSEGNA = "<p>Per sporgere un reclamo o segnalazione invia un <b>fax</b> al numero 0776 814169 o in alternativa compila on line l'apposito <b>form</b> \"Segnalazioni & Reclami\" disponibile sul sito www.stisrl.com</p><p>STI Srl si impegna a fornire, entro 5 giorni lavorativi, una prima risposta di presa in carico del reclamo, ed entro 30 giorni lavorativi dal ricevimento del reclamo, ad informare il reclamante in merito alle azioni intraprese per la sua gestione.</p> ";
 
 }

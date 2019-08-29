2d053ef19e63596488c7c2dbc90b0e791af5dcd7
antonio.dicivita <antonio.dicivita@ID0090.business.local>
2019-08-29 15:15:31 +0200

    commit intervento strumenti

--- AccPoint/src/it/portaleSTI/DTO/VerInterventoStrumentiDTO.java
+++ AccPoint/src/it/portaleSTI/DTO/VerInterventoStrumentiDTO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class VerInterventoStrumentiDTO implements Serializable{
 
 	private VerStrumentoDTO verStrumento;
-	private VerInterventoDTO verIntervento;
+	private int id_intervento;
 	private String ora_prevista;
 	
 	public VerStrumentoDTO getVerStrumento() {
@@ -14,17 +14,23 @@ public class VerInterventoStrumentiDTO implements Serializable{
 	public void setVerStrumento(VerStrumentoDTO verStrumento) {
 		this.verStrumento = verStrumento;
 	}	
-	public VerInterventoDTO getVerIntervento() {
-		return verIntervento;
-	}
-	public void setVerIntervento(VerInterventoDTO verIntervento) {
-		this.verIntervento = verIntervento;
-	}
+//	public VerInterventoDTO getVerIntervento() {
+//		return verIntervento;
+//	}
+//	public void setVerIntervento(VerInterventoDTO verIntervento) {
+//		this.verIntervento = verIntervento;
+//	}
 	public String getOra_prevista() {
 		return ora_prevista;
 	}
 	public void setOra_prevista(String ora_prevista) {
 		this.ora_prevista = ora_prevista;
+	}
+	public int getId_intervento() {
+		return id_intervento;
+	}
+	public void setId_intervento(int id_intervento) {
+		this.id_intervento = id_intervento;
 	}
 	
 	

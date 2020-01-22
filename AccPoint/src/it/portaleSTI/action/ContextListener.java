362da802e0e88923bb135c101dcc46dd3c17a3d8
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2020-01-22 15:08:46 +0100

    commit content listener

--- AccPoint/src/it/portaleSTI/action/ContextListener.java
+++ AccPoint/src/it/portaleSTI/action/ContextListener.java
@@ -92,7 +92,7 @@ public class ContextListener implements ServletContextListener {
 		Costanti.HOST_MAIL_SYSTEM_PWD=ac.decryptText(props.getProperty("HOST_MAIL_SYSTEM_PWD"), publicKey);
 		Costanti.HOST_MAIL_SYSTEM_SENDER=ac.decryptText(props.getProperty("HOST_MAIL_SYSTEM_SENDER"), publicKey);
 		Costanti.HOST_MAIL_SYSTEM_PORT=ac.decryptText(props.getProperty("HOST_MAIL_SYSTEM_PORT"), publicKey);
-		Costanti.MAIL_DEST_ALERT_PACCO=ac.decryptText(props.getProperty("MAIL_DEST_ALERT_PACCO"), publicKey);
+		Costanti.MAIL_DEST_ALERT_PACCO=props.getProperty("MAIL_DEST_ALERT_PACCO");
 		
 		System.out.println(Costanti.MAIL_DEST_ALERT_PACCO);
 		

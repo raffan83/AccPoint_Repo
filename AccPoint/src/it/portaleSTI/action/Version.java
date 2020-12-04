e90af787ebf0a0998df6c9ab5c085ac8adb77b22
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2020-12-04 09:20:29 +0100

    Add version

---
+++ AccPoint/src/it/portaleSTI/action/Version.java
@@ -0,0 +1,78 @@
+package it.portaleSTI.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import it.portaleSTI.DAO.SessionFacotryDAO;
+import it.portaleSTI.DTO.BachecaDTO;
+import it.portaleSTI.DTO.CompanyDTO;
+import it.portaleSTI.DTO.UtenteDTO;
+import it.portaleSTI.Exception.STIException;
+import it.portaleSTI.Util.Costanti;
+import it.portaleSTI.Util.Utility;
+import it.portaleSTI.bo.GestioneBachecaBO;
+import it.portaleSTI.bo.GestioneCompanyBO;
+import it.portaleSTI.bo.GestioneUtenteBO;
+
+/**
+ * Servlet implementation class GestioneBacheca
+ */
+@WebServlet("/version.do")
+public class Version extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Version() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("application/json");
+		JsonObject myObj = new JsonObject();
+		PrintWriter out = response.getWriter();
+		try{			
+
+				//generate JWT
+			//   	myObj.add("userObj", utente.getUtenteJsonObject(true));
+				myObj.addProperty("version", Costanti.DASM_VERSION);
+			   	
+		
+			
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}  
+				
+		out.println(myObj);
+	
+	}
+}

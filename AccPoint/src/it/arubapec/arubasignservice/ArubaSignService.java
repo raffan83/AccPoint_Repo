7f316df1f8a9f3626029c14b7ac95f0e76842973
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2020-12-29 12:51:15 +0100

    commit pades sign

--- AccPoint/src/it/arubapec/arubasignservice/ArubaSignService.java
+++ AccPoint/src/it/arubapec/arubasignservice/ArubaSignService.java
@@ -11,6 +11,10 @@ import org.apache.commons.io.FileUtils;
 import com.google.gson.JsonObject;
 
 import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.Auth;
+import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.PdfProfile;
+import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.PdfsignatureV2;
+import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.PdfsignatureV2E;
+import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.PdfsignatureV2ResponseE;
 import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.Pkcs7SignV2;
 import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.Pkcs7SignV2E;
 import it.arubapec.arubasignservice.ArubaSignServiceServiceStub.Pkcs7SignV2ResponseE;
@@ -208,12 +212,16 @@ public static JsonObject signDocumento(String utente, String filename) throws Ty
 		javax.activation.DataHandler dh = new DataHandler(uri.toURL());
 		
 		sign.setBinaryinput(dh);
+		
+		
 
 		sign.setTransport(TypeTransport.BYNARYNET);
 		
+	
 		pkcs.setSignRequestV2(sign);
 		
 		request.setPkcs7SignV2(pkcs);
+		
 		
 		Pkcs7SignV2ResponseE response= stub.pkcs7SignV2(request);
 		JsonObject jsonObj = new JsonObject();
@@ -241,6 +249,79 @@ public static JsonObject signDocumento(String utente, String filename) throws Ty
 		 
 	}
 
+public static JsonObject signDocumentoPades(String utente, String filename) throws TypeOfTransportNotImplementedException, IOException {
+	
+
+	ArubaSignServiceServiceStub stub = new ArubaSignServiceServiceStub();
+	
+	
+	PdfsignatureV2E request= new PdfsignatureV2E();
+	PdfsignatureV2 pkcs = new PdfsignatureV2();
+	
+	SignRequestV2  sign = new SignRequestV2();
+	
+	
+	sign.setCertID("AS0");
+	
+	Auth identity = new Auth();
+	identity.setDelegated_domain("faSTI");
+	identity.setTypeOtpAuth("faSTI");
+	identity.setOtpPwd("dsign");
+	identity.setTypeOtpAuth("faSTI");
+	
+	identity.setUser(utente);
+	
+	identity.setDelegated_user("admin.firma");
+	identity.setDelegated_password("uBFqc8YYslTG");
+	
+	sign.setIdentity(identity);
+
+	String path = Costanti.PATH_FIRMA_DIGITALE+filename;
+	File f = new File(path);
+
+		URI uri = f.toURI();
+	
+	javax.activation.DataHandler dh = new DataHandler(uri.toURL());
+	
+	sign.setBinaryinput(dh);
+	
+	
+
+	sign.setTransport(TypeTransport.BYNARYNET);
+	
+
+	pkcs.setSignRequestV2(sign);
+	pkcs.setPdfprofile(PdfProfile.BASIC);
+	
+	
+	request.setPdfsignatureV2(pkcs);
+	
+	
+	PdfsignatureV2ResponseE response= stub.pdfsignatureV2(request);
+	JsonObject jsonObj = new JsonObject();
+	
+
+
+	if( response.getPdfsignatureV2Response().get_return().getStatus().equals("KO")) {
+		jsonObj.addProperty("success", false);
+		jsonObj.addProperty("messaggio", response.getPdfsignatureV2Response().get_return().getDescription());
+	}else {
+		
+		jsonObj.addProperty("success", true);
+		String fileNoExt = filename.substring(0, filename.length()-4);
+		DataHandler fileReturn=response.getPdfsignatureV2Response().get_return().getBinaryoutput();
+		File targetFile = new File(Costanti.PATH_FIRMA_DIGITALE+ fileNoExt+".pdf");
+		FileUtils.copyInputStreamToFile(fileReturn.getInputStream(), targetFile);
+
+		jsonObj.addProperty("messaggio", "Documento firmato");
+	}
+	
+//	f.delete();
+	return jsonObj;
+	
+	
+	 
+}
 
 
 }

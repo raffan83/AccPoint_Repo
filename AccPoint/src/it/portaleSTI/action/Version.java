830a982ad6dd82348792d85a2ead4f7d1ecdce6f
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2021-05-11 12:30:45 +0200

    commit version

--- AccPoint/src/it/portaleSTI/action/Version.java
+++ AccPoint/src/it/portaleSTI/action/Version.java
@@ -34,7 +34,7 @@ import it.portaleSTI.bo.GestioneUtenteBO;
 @WebServlet("/version.do")
 public class Version extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String DASM_VERSION = "2.3.1";
+	private static final String DASM_VERSION = "2.3.2";
 	private static final String DASM_VER_VERSION ="2.0.5";
        
     /**

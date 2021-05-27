18118c365b427e6e226d56a8698e6faadcc162b2
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2021-05-27 11:19:11 +0200

    aggiornamento versione DasmTar

--- AccPoint/src/it/portaleSTI/action/Version.java
+++ AccPoint/src/it/portaleSTI/action/Version.java
@@ -34,7 +34,7 @@ import it.portaleSTI.bo.GestioneUtenteBO;
 @WebServlet("/version.do")
 public class Version extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String DASM_VERSION = "2.3.3";
+	private static final String DASM_VERSION = "2.3.4";
 	private static final String DASM_VER_VERSION ="2.0.5";
        
     /**

251cf713a440bf3e910c19dfce9039d6f3a712f4
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2022-01-11 12:31:08 +0100

    commit

--- AccPoint/src/it/portaleSTI/action/AbbinaSchede.java
+++ AccPoint/src/it/portaleSTI/action/AbbinaSchede.java
@@ -54,6 +54,7 @@ public class AbbinaSchede extends HttpServlet {
 		response.setContentType("text/html");
 		 
 		try {
+			  
 			
 
 			ArrayList<TipoStrumentoDTO> listaTipoStrumento = (ArrayList<TipoStrumentoDTO>) GestioneStrumentoBO.getListaTipoStrumento();

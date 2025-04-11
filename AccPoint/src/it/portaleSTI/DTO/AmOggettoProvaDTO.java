ecd25a821d58c650877027192a92a939141a9ebd
raffaele.fantini <raffaele.fantini@ID0093.enterprise.local>
2025-04-11 15:30:29 +0200

    Oggetto in prova hb

---
+++ AccPoint/src/it/portaleSTI/DTO/AmOggettoProvaDTO.java
@@ -0,0 +1,142 @@
+package it.portaleSTI.DTO;
+
+public class AmOggettoProvaDTO {
+
+    private int id;
+    private String zonaRifFasciame;
+    private String spessoreFondo;
+    private String tipo;
+    private String volume;
+    private String materialeFasciame;
+    private String pressione;
+    private String costruttore;
+    private String nFabbrica;
+    private String zonaRifFondo;
+    private int anno;
+    private String spessoreFasciame;
+    private String materialeFondo;
+    private String matricola;
+    private String descrizione;
+
+    // Getter e Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getZonaRifFasciame() {
+        return zonaRifFasciame;
+    }
+
+    public void setZonaRifFasciame(String zonaRifFasciame) {
+        this.zonaRifFasciame = zonaRifFasciame;
+    }
+
+    public String getSpessoreFondo() {
+        return spessoreFondo;
+    }
+
+    public void setSpessoreFondo(String spessoreFondo) {
+        this.spessoreFondo = spessoreFondo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getMaterialeFasciame() {
+        return materialeFasciame;
+    }
+
+    public void setMaterialeFasciame(String materialeFasciame) {
+        this.materialeFasciame = materialeFasciame;
+    }
+
+    public String getPressione() {
+        return pressione;
+    }
+
+    public void setPressione(String pressione) {
+        this.pressione = pressione;
+    }
+
+    public String getCostruttore() {
+        return costruttore;
+    }
+
+    public void setCostruttore(String costruttore) {
+        this.costruttore = costruttore;
+    }
+
+    public String getnFabbrica() {
+        return nFabbrica;
+    }
+
+    public void setnFabbrica(String nFabbrica) {
+        this.nFabbrica = nFabbrica;
+    }
+
+    public String getZonaRifFondo() {
+        return zonaRifFondo;
+    }
+
+    public void setZonaRifFondo(String zonaRifFondo) {
+        this.zonaRifFondo = zonaRifFondo;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public String getSpessoreFasciame() {
+        return spessoreFasciame;
+    }
+
+    public void setSpessoreFasciame(String spessoreFasciame) {
+        this.spessoreFasciame = spessoreFasciame;
+    }
+
+    public String getMaterialeFondo() {
+        return materialeFondo;
+    }
+
+    public void setMaterialeFondo(String materialeFondo) {
+        this.materialeFondo = materialeFondo;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+}
